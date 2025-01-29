f7b0a22436b33e15aefe6ab46c3d277672fcae41
Peter Maltby <contact@petermaltby.com>
2025-01-29 21:07:09 +0000

    traingle

--- src/app/render/mod.rs
+++ src/app/render/mod.rs
@@ -3,8 +3,8 @@ use wgpu::{BackendOptions, Backends, InstanceDescriptor, InstanceFlags, SurfaceT
 use wgpu::{Device, Queue, Surface, SurfaceConfiguration};
 use winit::window::Window;
 
-use config::Config;
 use crate::prelude::*;
+use config::Config;
 use error::{Error, Result};
 
 pub mod config;
@@ -16,6 +16,7 @@ pub struct Renderer<'window> {
     queue: Queue,
     window: Arc<Window>,
     context_configuration: SurfaceConfiguration,
+    render_pipeline: wgpu::RenderPipeline,
     size: winit::dpi::PhysicalSize<u32>,
 }
 
@@ -66,9 +67,14 @@ impl<'window> Renderer<'window> {
         let size = winit::dpi::PhysicalSize::new(800, 800);
 
         let context_capabilities = context.get_capabilities(&adapter);
-        
+
         // TODO set some nice defaults
-        let surface_format = context_capabilities.formats.iter().find(|f| f.is_srgb()).copied().unwrap_or(context_capabilities.formats[0]);
+        let surface_format = context_capabilities
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(context_capabilities.formats[0]);
 
         let context_configuration = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
@@ -81,7 +87,57 @@ impl<'window> Renderer<'window> {
             desired_maximum_frame_latency: 2,
         };
 
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shaders/shader.wgsl").into()),
+        });
+
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Render Piepline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("renderPipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: context_configuration.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+            cache: None,
+        });
+
         Ok(Self {
+            render_pipeline,
             window,
             context,
             device,
@@ -121,8 +177,8 @@ impl<'window> Renderer<'window> {
         let context_texture = surface_texture.texture.create_view(&wgpu::TextureViewDescriptor::default());
         let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
         {
-            let _r_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: None,
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("main render pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     // output to texture
                     view: &context_texture,
@@ -141,6 +197,9 @@ impl<'window> Renderer<'window> {
                 timestamp_writes: None,
                 occlusion_query_set: None,
             });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
         self.queue.submit(Some(encoder.finish()));
         self.window.as_ref().request_redraw();
